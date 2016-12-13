2997a48109001689934a7453cd70dc8bf1b0bad3
Robert Arbon <ra15808@newblue4.cm.cluster>
2016-12-13 21:05:32 +0000

    Finished equilibration

---
+++ 6_equilibration/restrained_equilibration/equilibration.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -l walltime=24:00:00
+#PBS -q gpu
+#PBS -l nodes=1:ppn=1:gpus=1
+#PBS -j oe
+#PBS -N AADH_equilibration
+cd $PBS_O_WORKDIR
+echo Running on host `hostname`
+echo Time is `date`
+echo Directory is `pwd`
+echo PBS job ID is $PBS_JOBID
+echo `cat $PBS_NODEFILE | uniq`
+charmm_dir=/panfs/panasas01/chem/ra15808/c41a2/exec/gnu
+input=equilibration
+$charmm_dir/charmm -i $input.inp > $input.out
